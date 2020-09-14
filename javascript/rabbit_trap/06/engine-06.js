b63a58b977d17719f8fabb40ee41d8d3db9ce9f8
jayone <imdff0803@gmail.com>
2020-09-14 22:01:38 +0900

    rabbit_trap - part6: Loading Levels

---
+++ javascript/rabbit_trap/06/engine-06.js
@@ -0,0 +1,53 @@
+const Engine = function(time_step, update, render) {
+
+    this.accumulated_time = 0;  // Amount of time that's accumulated since the last update
+    this.animation_frame_request = undefined;  // reference to the AFR
+    this.time = undefined;  // The most recent timestamp of loop execution
+    this.time_step = time_step;  // 1000/30 = 30 frames per second
+
+    this.updated = false;  // Whether or not the update function has been called since the last cycle
+
+    this.update = update;  // The update function
+    this.render = render;  // The render function
+
+    this.run = function(time_stamp) { // This is one cycle of the game loop
+        this.animation_frame_request = window.requestAnimationFrame(this.handleRun);
+        
+        this.accumulated_time += time_stamp - this.time;
+        this.time = time_stamp;
+
+        // update 호출하는 데 시간이 오래 걸리는 경우 cpu 부하 방지
+        if (this.accumulated_time >= this.time_step * 3) {
+            this.accumulated_time = this.time_step;
+        }
+
+        while(this.accumulated_time >= this.time_step) {
+            this.accumulated_time -= this.time_step;
+            this.update(time_stamp);
+            this.updated = true;  // If the game has updated, we need to draw it again
+        }
+
+        /* This allows us to only draw when the game has updated. */
+        if (this.updated) {
+            this.updated = false;
+            this.render(time_stamp);
+        }
+
+    };
+
+    this.handleRun = (time_step) => { this.run(time_step); };
+};
+
+Engine.prototype = {
+    constructor: Engine,
+
+    start: function() {
+        this.accumulated_time = this.time_step;
+        this.time = window.performance.now();
+        this.animation_frame_request = window.requestAnimationFrame(this.handleRun)
+    },
+
+    stop: function() {
+        window.cancelAnimationFrame(this.animation_frame_request);
+    }
+};
---
+++ javascript/rabbit_trap/README.md
@@ -0,0 +1,7 @@
+Videos From : https://www.youtube.com/watch?v=opiWzi0KWjs&list=PLcN6MkgfgN4CpMUgWEM5d70ANMWgcmBp8&index=1
+Source Code From : https://github.com/pothonprogramming/pothonprogramming.github.io/tree/master/content/rabbit-trap
+
+### 간단한 웹 서버 실행
+```python
+python -m http.server
+```
