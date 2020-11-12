b666bc26c4aa496ad6cbbfd0403cfbc9582ca36d
jayone <imdff0803@gmail.com>
2020-11-12 20:07:36 +0900

    flask-tdd-docker: Part 1 - Initialize

---
+++ webapp/flask-tdd-docker/README.md
@@ -0,0 +1,2 @@
+Source from testdriven.io Course
+- [Test-Driven Development with Python, Flask and Docker](https://testdriven.io/courses/tdd-flask/)
---
+++ webapp/flask-tdd-docker/project/__init__.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, jsonify
+from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+
+
+# instantiate the db
+db = SQLAlchemy()
+
+
+def create_app(script_info=None):
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_setting = os.getenv("APP_SETTINGS")
+    app.config.from_object(app_setting)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.ping import ping_blueprint
+    from project.api.users import users_blueprint
+
+    app.register_blueprint(ping_blueprint)
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {"app": app, "db": db}
+
+    return app
