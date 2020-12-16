c92ecd87487fc27eb843e33caca1795478ca3029
jayone <imdff0803@gmail.com>
2020-12-17 00:23:53 +0900

    flask-react-auth: test_auth_unit

--- webapp/flask-react-auth/services/users/src/tests/conftest.py
+++ webapp/flask-react-auth/services/users/src/tests/conftest.py
@@ -4,6 +4,12 @@ from src import create_app, db
 from src.api.users.models import User
 
 
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
 @pytest.fixture(scope="module")
 def test_app():
     app = create_app()
@@ -20,7 +26,7 @@ def test_database():
     db.drop_all()
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def add_user():
     def _add_user(username, email, password):
         user = User(username=username, email=email, password=password)
@@ -29,3 +35,8 @@ def add_user():
         return user
 
     return _add_user
+
+
+@pytest.fixture(scope="function")
+def attr_dict():
+    return AttrDict()
---
+++ webapp/flask-react-auth/services/users/src/tests/test_auth_unit.py
@@ -0,0 +1,309 @@
+import json
+import bcrypt
+import pytest
+from flask import current_app
+
+import src.api.users.auth
+from src.api.users.models import User
+
+
+def test_user_registration(test_app, monkeypatch):
+    def mock_get_user_by_email(email):
+        return None
+
+    def mock_add_user(username, email, password):
+        return User(username=username, email=email, password=password)
+
+    monkeypatch.setattr(src.api.users.auth, "get_user_by_email", mock_get_user_by_email)
+    monkeypatch.setattr(src.api.users.auth, "add_user", mock_add_user)
+
+    client = test_app.test_client()
+    resp = client.post(
+        "/auth/register",
+        data=json.dumps(
+            {"username": "justatest", "email": "test@test.com", "password": "123456"},
+        ),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 201
+    assert resp.content_type == "application/json"
+    assert "justatest" in data["username"]
+    assert "test@test.com" in data["email"]
+    assert "password" not in data
+
+
+def test_user_registration_duplicate_email(test_app, monkeypatch):
+    def mock_get_user_by_email(email):
+        return True
+
+    monkeypatch.setattr(src.api.users.auth, "get_user_by_email", mock_get_user_by_email)
+
+    client = test_app.test_client()
+    resp = client.post(
+        "/auth/register",
+        data=json.dumps(
+            {"username": "michael", "email": "test@test.com", "password": "test"}
+        ),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert resp.content_type == "application/json"
+    assert "Sorry. That email already exists." in data["message"]
+
+
+@pytest.mark.parametrize(
+    "payload",
+    [
+        {},
+        {"email": "me@testdriven.io", "password": "greaterthanten"},
+        {"username": "michael", "password": "greaterthanten"},
+        {"email": "me@testdriven.io", "username": "michael"},
+    ],
+)
+def test_user_registration_invalid_json(test_app, payload):
+    client = test_app.test_client()
+    resp = client.post(
+        "/auth/register",
+        data=json.dumps(payload),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert resp.content_type == "application/json"
+    assert "Input payload validation failed" in data["message"]
+
+
+def test_registered_user_login(test_app, monkeypatch):
+    def mock_get_user_by_email(email):
+        return User(username="test3", email="test3@test.com", password="test")
+
+    monkeypatch.setattr(src.api.users.auth, "get_user_by_email", mock_get_user_by_email)
+
+    client = test_app.test_client()
+    resp = client.post(
+        "/auth/login",
+        data=json.dumps({"email": "test3@test.com", "password": "test"}),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert resp.content_type == "application/json"
+    assert data["access_token"]
+    assert data["refresh_token"]
+
+
+def test_not_registered_user_login(test_app, monkeypatch):
+    def mock_get_user_by_email(email):
+        return None
+
+    monkeypatch.setattr(src.api.users.auth, "get_user_by_email", mock_get_user_by_email)
+
+    client = test_app.test_client()
+    resp = client.post(
+        "/auth/login",
+        data=json.dumps({"email": "testnotreal@test.com", "password": "test"}),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert resp.content_type == "application/json"
+    assert "User does not exist." in data["message"]
+
+
+def test_valid_refresh(test_app, monkeypatch):
+    def mock_get_user_by_email(email):
+        return User(username="test4", email="test4@test.com", password="test")
+
+    def mock_get_user_by_id(user_id):
+        return User(username="test4", email="test4@test.com", password="test")
+
+    monkeypatch.setattr(src.api.users.auth, "get_user_by_email", mock_get_user_by_email)
+    monkeypatch.setattr(src.api.users.auth, "get_user_by_id", mock_get_user_by_id)
+
+    client = test_app.test_client()
+    # user login
+    resp_login = client.post(
+        "/auth/login",
+        data=json.dumps({"email": "test4@test.com", "password": "test"}),
+        content_type="application/json",
+    )
+    # valid refresh
+    refresh_token = json.loads(resp_login.data.decode())["refresh_token"]
+    resp = client.post(
+        "/auth/refresh",
+        data=json.dumps({"refresh_token": refresh_token}),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert resp.content_type == "application/json"
+    assert data["access_token"]
+    assert data["refresh_token"]
+    assert resp.content_type == "application/json"
+
+
+def test_invalid_refresh_expired_token(test_app, monkeypatch):
+    def mock_get_user_by_email(email):
+        return User(username="test5", email=email, password="test")
+
+    monkeypatch.setattr(src.api.users.auth, "get_user_by_email", mock_get_user_by_email)
+
+    test_app.config["REFRESH_TOKEN_EXPIRATION"] = -1
+    client = test_app.test_client()
+    # user login
+    resp_login = client.post(
+        "/auth/login",
+        data=json.dumps({"email": "test5@test.com", "password": "test"}),
+        content_type="application/json",
+    )
+    # invalid token refresh
+    refresh_token = json.loads(resp_login.data.decode())["refresh_token"]
+    resp = client.post(
+        "/auth/refresh",
+        data=json.dumps({"refresh_token": refresh_token}),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 401
+    assert resp.content_type == "application/json"
+    assert "Signature expired. Please log in again." in data["message"]
+
+    test_app.config["REFRESH_TOKEN_EXPIRATION"] = 3
+
+
+def test_refresh_not_exist_user(test_app, monkeypatch):
+    def mock_get_user_by_id(user_id):
+        return None
+
+    monkeypatch.setattr(src.api.users.auth, "get_user_by_id", mock_get_user_by_id)
+
+    client = test_app.test_client()
+    refresh_token = User.encode_token(999, "refresh")
+    resp = client.post(
+        "/auth/refresh",
+        data=json.dumps({"refresh_token": refresh_token.decode()}),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 401
+    assert resp.content_type == "application/json"
+    assert "Invalid token" in data["message"]
+
+
+def test_invalid_refresh(test_app):
+    client = test_app.test_client()
+    resp = client.post(
+        "/auth/refresh",
+        data=json.dumps({"refresh_token": "Invalid"}),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 401
+    assert resp.content_type == "application/json"
+    assert "Invalid token. Please log in again." in data["message"]
+
+
+def test_user_status(test_app, monkeypatch):
+    def mock_get_user_by_email(email):
+        user = User(username="test6", email=email, password="test")
+        user.id = 1
+        return user
+
+    def mock_get_user_by_id(user_id):
+        return User(username="test6", email="test6@test.com", password="test")
+
+    monkeypatch.setattr(src.api.users.auth, "get_user_by_email", mock_get_user_by_email)
+    monkeypatch.setattr(src.api.users.auth, "get_user_by_id", mock_get_user_by_id)
+
+    client = test_app.test_client()
+    resp_login = client.post(
+        "/auth/login",
+        data=json.dumps({"email": "test6@test.com", "password": "test"}),
+        content_type="application/json",
+    )
+    token = json.loads(resp_login.data.decode())["access_token"]
+    resp = client.get(
+        "/auth/status",
+        headers={"Authorization": f"Bearer {token}"},
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert resp.content_type == "application/json"
+    assert "test6" in data["username"]
+    assert "test6@test.com" in data["email"]
+    assert "password" not in data
+
+
+def test_invalid_status(test_app):
+    client = test_app.test_client()
+    resp = client.get(
+        "/auth/status",
+        headers={"Authorization": "Bearer invalid"},
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 401
+    assert resp.content_type == "application/json"
+    assert "Invalid token. Please log in again." in data["message"]
+
+
+def test_user_status_not_exist(test_app, monkeypatch):
+    def mock_get_user_by_id(user_id):
+        return None
+
+    monkeypatch.setattr(src.api.users.auth, "get_user_by_id", mock_get_user_by_id)
+
+    client = test_app.test_client()
+    access_token = User.encode_token(999, "access").decode()
+    resp = client.get(
+        "/auth/status",
+        headers={"Authorization": f"Bearer {access_token}"},
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 401
+    assert resp.content_type == "application/json"
+    assert "Invalid token" in data["message"]
+
+
+def test_user_status_token_expired(test_app, monkeypatch):
+    def mock_get_user_by_email(email):
+        return User(username="test", email="test@test.com", password="test")
+
+    monkeypatch.setattr(src.api.users.auth, "get_user_by_email", mock_get_user_by_email)
+    
+    test_app.config["ACCESS_TOKEN_EXPIRATION"] = -1
+    client = test_app.test_client()
+    # user login
+    resp_login = client.post(
+        "/auth/login",
+        data=json.dumps({"email": "test7@test.com", "password": "test"}),
+        content_type="application/json",
+    )
+    # expired token
+    access_token = json.loads(resp_login.data.decode())["access_token"]
+    resp = client.get(
+        "/auth/status",
+        headers={"Authorization": f"Bearer {access_token}"},
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 401
+    assert resp.content_type == "application/json"
+    assert "Signature expired. Please log in again." in data["message"]
+
+    test_app.config["ACCESS_TOKEN_EXPIRATION"] = 3
+
+
+def test_user_status_without_header(test_app):
+    client = test_app.test_client()
+    resp = client.get(
+        "/auth/status",
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 403
+    assert "Token required" in data["message"]
