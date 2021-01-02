732aeddc53b5ae91067268c91e4491f8d6fb76f6
jayone <imdff0803@gmail.com>
2021-01-03 01:44:00 +0900

    flask-react-auth: React Authentication - part 2

---
+++ webapp/flask-react-auth/services/client/src/components/UserStatus.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { Redirect } from 'react-router-dom';
+import PropTypes from "prop-types";
+import axios from "axios";
+
+class UserStatus extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      username: "",
+    };
+  }
+
+  componentDidMount() {
+    this.getUserStatus();
+  }
+
+  getUserStatus(event) {
+    const options = {
+      url: `${process.env.REACT_APP_API_SERVICE_URL}/auth/status`,
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.props.accessToken}`,
+      },
+    };
+
+    return axios(options)
+      .then((res) => {
+        this.setState({
+          email: res.data.email,
+          username: res.data.username
+        })
+        // console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    if (!this.props.isAuthenticated()) {
+      return <Redirect to="/login" />;
+    }
+    return (
+      <div>
+        <ul>
+          <li><strong>Email:</strong>&nbsp;<span data-testid="user-email">{this.state.email}</span></li>
+          <li><strong>Username:</strong>&nbsp;<span data-testid="user-username">{this.state.username}</span></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+UserStatus.propTypes = {
+  accessToken: PropTypes.string,
+  isAuthenticated: PropTypes.func.isRequired
+};
+
+export default UserStatus;
